3a805f20a9904409c6d868bad7ba886ef80d5a23
AulonaBinaj <aulonabinaj2@gmail.com>
2025-11-24 12:25:10 +0100

    added express js with some little examples

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
 const http = require("http");
-const routes = require("./routes");
-console.log(routes.someText);
-const server = http.createServer(routes.handler);
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/", (req, res, next) => {
+  console.log("This always runs");
+  next(); // allows the request to continue to the next middleware in line
+});
+
+app.use("/add-product", (req, res, next) => {
+  console.log("In another middleware");
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type=submit>Add Product</button></input></form>'
+  );
+});
+
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  console.log("In another middleware");
+  res.send("<h1>Hello from express!</h1>");
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
